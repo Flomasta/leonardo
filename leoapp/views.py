054470ea199f1c01d2f0578df74b0989d6468825
flomasta <zatevi@mail.ru>
2023-02-12 14:11:32 +0300

    initial commint dashboard branch temlate view

--- leoapp/views.py
+++ leoapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Items, PriceDynamic
+
 
 # Create your views here.
+
+def dashboard(request):
+    prices = PriceDynamic.objects.select_related('item_id').all()
+    return render(request, 'index.html', {'prices': prices})
--- leonardo/settings.py
+++ leonardo/settings.py
@@ -126,8 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+MEDIA_ROOT = BASE_DIR / 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
